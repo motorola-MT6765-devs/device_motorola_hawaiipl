916c329b3861d54a1f52945fbe3ec4745f0be8f6
kaikeEXE <kaikepinheio@gmail.com>
2023-01-26 13:44:12 +0000

    hawaiip: Remove `init.recovery` from rootdir
    Not needed cause already set in recovery/root

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -6,12 +6,4 @@
 
 LOCAL_PATH := $(call my-dir)
 
-# Recovery init configuration files
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.recovery.mt6765.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.recovery.mt6765.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
 
